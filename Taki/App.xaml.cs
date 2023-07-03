4300398c5798e9d98fe216259b360f0b254974b8
Daniel Monettelli <danielmonetelli@hotmail.com>
2023-07-03 10:05:39 -0500

    🎉 [Code] Added initial .NET MAUI template

---
+++ Taki/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Taki;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Taki/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Taki;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
